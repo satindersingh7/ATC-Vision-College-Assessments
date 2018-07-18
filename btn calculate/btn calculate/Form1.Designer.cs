e59a206856872391dfa5533456e534e23cc5e731
satindersingh7 <41350833+satindersingh7@users.noreply.github.com>
2018-07-18 13:56:07 +1200

    Add files via upload

---
+++ btn calculate/btn calculate/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace btn_calculate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGrade = new System.Windows.Forms.Label();
+            this.txtGrade = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lbxGrade = new System.Windows.Forms.ListBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lbxName = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblGrade
+            // 
+            this.lblGrade.AutoSize = true;
+            this.lblGrade.Location = new System.Drawing.Point(382, 34);
+            this.lblGrade.Name = "lblGrade";
+            this.lblGrade.Size = new System.Drawing.Size(64, 13);
+            this.lblGrade.TabIndex = 0;
+            this.lblGrade.Text = "Enter Grade";
+            // 
+            // txtGrade
+            // 
+            this.txtGrade.Location = new System.Drawing.Point(368, 66);
+            this.txtGrade.Name = "txtGrade";
+            this.txtGrade.Size = new System.Drawing.Size(151, 20);
+            this.txtGrade.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(195, 28);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(96, 58);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate This!";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbxGrade
+            // 
+            this.lbxGrade.FormattingEnabled = true;
+            this.lbxGrade.Location = new System.Drawing.Point(195, 173);
+            this.lbxGrade.Name = "lbxGrade";
+            this.lbxGrade.Size = new System.Drawing.Size(120, 95);
+            this.lbxGrade.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(368, 134);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(151, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(383, 104);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(63, 13);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "Enter Name";
+            this.lblName.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lbxName
+            // 
+            this.lbxName.FormattingEnabled = true;
+            this.lbxName.Location = new System.Drawing.Point(368, 173);
+            this.lbxName.Name = "lbxName";
+            this.lbxName.Size = new System.Drawing.Size(137, 95);
+            this.lbxName.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(195, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 34);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Location = new System.Drawing.Point(598, 66);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(0, 13);
+            this.lblAverage.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(557, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbxName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lbxGrade);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtGrade);
+            this.Controls.Add(this.lblGrade);
+            this.Name = "Form1";
+            this.Text = "Calculate This!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGrade;
+        private System.Windows.Forms.TextBox txtGrade;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.ListBox lbxGrade;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.ListBox lbxName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ btn calculate/btn calculate/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace btn_calculate
+{
+    public partial class Form1 : Form
+    {
+        private Single Grade, Max, Min;
+        private int Count = 0;
+        float GradeAverage =0, GradeAverageTotal;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToSingle(txtGrade.Text) < 50)
+            {
+                lblGrade.Text = "Fail";
+                lbxGrade.Items.Add(txtGrade.Text + " Fail");
+            }
+            else
+            {
+                lblGrade.Text = "Pass";
+                lbxGrade.Items.Add(txtGrade.Text + " Pass");
+            }
+            }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            {
+                try
+                {
+                    Grade = Convert.ToSingle(txtGrade.Text);
+                    GradeAverage = GradeAverage + Grade; //add grade to the variable and get a running total
+                    Count = Count + 1; //count how many times the button is clicked
+                    GradeAverageTotal = GradeAverage / Count;
+                    lblAverage.Text = Convert.ToString(GradeAverageTotal);
+                }
+        } }
+    }
+}
+
